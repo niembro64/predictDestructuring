1a193e21bf39966a0a22a336c7c1b64184990119
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-08 21:49:34 -0500

    Done

--- predictDestructuring.js
+++ predictDestructuring.js
@@ -21,14 +21,13 @@ const employee = {
 }
 const { name: otherName } = employee;
 
-
 // Predict the output
 // console.log(name);
 // error
 console.log(otherName);
+// Elon
 
 // Problem 3
-
 const person = {
     name: 'Phil Smith',
     age: 47,
@@ -36,20 +35,23 @@ const person = {
 }
 const password = '12345';
 const { password: hashedPassword } = person;  
-
 // Predict the output
 console.log(password);
+// 12345
 console.log(hashedPassword);
+// Error
 
 // Problem 4
 const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
 const [,first] = numbers;
 const [,,,second] = numbers;
 const [,,,,,,,,third] = numbers;
-
 // Predict the output
 console.log(first == second);
+// false
 console.log(first == third);
+// true
+
 
 // Problem 5
 const lastTest = {
@@ -59,10 +61,12 @@ const lastTest = {
 const { key } = lastTest;
 const { secondKey } = lastTest;
 const [ ,willThisWork] = secondKey;
-
 // Predict the output
 console.log(key);
+// value
 console.log(secondKey);
+// 151833
 console.log(secondKey[0]);
+// 1
 console.log(willThisWork);
-
+// 5
